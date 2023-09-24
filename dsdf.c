f79e6411414e576cfa43fe6278ee48c239811ee4
aymey <fabianpaci@gmail.com>
2023-09-24 17:11:47 +1000

    remove abitrary amount cause of bad way to draw

--- dsdf.c
+++ dsdf.c
@@ -30,18 +30,6 @@ void add_shape(bool type) {
         type,
         size
     };
-
-    if(drawing[used-1].pos.x == drawing[used].pos.x &&
-        drawing[used-1].pos.y == drawing[used].pos.y &&
-        drawing[used-1].type == drawing[used].type &&
-        drawing[used-1].size == drawing[used].size) {
-        used--;
-        drawing[used] = (Shape){
-            (Vector2){0,0},
-            0,
-            0
-        };
-    }
 }
 
 int point_dist(unsigned int index) {
@@ -49,35 +37,16 @@ int point_dist(unsigned int index) {
 }
 
 void draw_distance(void) {
-    if(amount == 100)
-        amount = used;
-
     unsigned int closest = used;
     for(int i = 1; i < used; i++) { // WHY ONE????
         if(point_dist(i) < point_dist(closest))
            closest = i;
     }
 
-    for(int i = 1; i < amount; i++) {
-        closest = i;
-        DrawLineV(GetMousePosition(), drawing[closest].pos, WHITE);
-        unsigned int distance = point_dist(closest);
-        Vector2 midpoint = {GetMouseX()-(GetMouseX()-drawing[closest].pos.x)/2, GetMouseY()-(GetMouseY()-drawing[closest].pos.y)/2};
-        DrawText(TextFormat("%d", distance), midpoint.x, midpoint.y, size, WHITE);
-    }
-
-    if(amount == used)
-        amount = 100;
-}
-
-void draw_slider(bool colliding) {
-    if(colliding) {
-        float sliderValue = (float)(GetMouseX() - slider.x) / slider.width;
-        amount = (int)(sliderValue * 100);
-    }
-    DrawRectangleRec(slider, LIGHTGRAY);
-    DrawRectangle(slider.x + slider.width * (amount / 100.0f) - 5, slider.y - 5, 10, slider.height + 10, MAROON);
-    DrawText(TextFormat("amount: %s", amount == 100 ? "All" : TextFormat("%d", amount)), 10, 10, 20, WHITE);
+    DrawLineV(GetMousePosition(), drawing[closest].pos, WHITE);
+    unsigned int distance = point_dist(closest);
+    Vector2 midpoint = {GetMouseX()-(GetMouseX()-drawing[closest].pos.x)/2, GetMouseY()-(GetMouseY()-drawing[closest].pos.y)/2};
+    DrawText(TextFormat("%d", distance), midpoint.x, midpoint.y, size, WHITE);
 }
 
 void draw(bool type) {
@@ -102,24 +71,18 @@ int main(void) {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(0, 0, "dsdf");
     bool type = true;
-    bool colliding = false;
     while (!WindowShouldClose()) {
         size += GetMouseWheelMove()*2;
         if(size <= 0)
             size = 0;
         if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
             type = !type;
-        if(CheckCollisionPointRec(GetMousePosition(), slider) && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-            colliding = true;
-        else
-            colliding = false;
-        if(!colliding && size && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        if(size && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
             add_shape(type);
 
         BeginDrawing();
             ClearBackground(BACKGROUND);
             draw(type);
-            draw_slider(colliding);
         EndDrawing();
     }
 
